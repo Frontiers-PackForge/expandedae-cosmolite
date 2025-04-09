b65a8e74453434cb28e38f4c024604a38726216a
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-04-10 01:02:27 +0200

    ok

--- src/main/java/lu/kolja/expandedae/Platform.java
+++ src/main/java/lu/kolja/expandedae/Platform.java
@@ -1,11 +1,16 @@
 package lu.kolja.expandedae;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import appeng.core.AppEng;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Platform {
+    Logger log = LoggerFactory.getLogger(Platform.class);
+
     public static void registerMenuType(String id, MenuType<?> menuType) {
         ForgeRegistries.MENU_TYPES.register(AppEng.makeId(id), menuType);
     }
+
 }
